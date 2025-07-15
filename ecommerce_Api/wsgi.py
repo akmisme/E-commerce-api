c3323b944b6d7796c5a88c4bb09fc5212835be12
akmisme <aungkaungmyat2318@gmail.com>
2025-07-15 22:21:34 +0630

    update

--- ecommerce_Api/wsgi.py
+++ ecommerce_Api/wsgi.py
@@ -1,16 +1,19 @@
-"""
-WSGI config for ecommerce_Api project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
+#import os
+#from django.core.wsgi import get_wsgi_application
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE','ecommerce_Api.settings')
+#application = get_wsgi_application()
 
 import os
-
+from django.conf import settings
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 from django.core.wsgi import get_wsgi_application
+from static_ranges import Ranges
+from dj_static import Cling, MediaCling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_Api.settings')
 
-application = get_wsgi_application()
+if settings.DEBUG:
+    application = StaticFilesHandler(get_wsgi_application())
+else:
+    application = get_wsgi_application()
+    application = Ranges(Cling(MediaCling(get_wsgi_application())))
